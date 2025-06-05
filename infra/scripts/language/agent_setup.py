9e91ebb94e61b2e4908b684ccf0be33fe1917744
Annabel Ng <annabelng@berkeley.edu>
2025-06-05 16:09:41 -0700

    update agent instructions

--- infra/scripts/language/agent_setup.py
+++ infra/scripts/language/agent_setup.py
@@ -55,17 +55,25 @@ with agents_client:
 
     # Define the instructions for the agent
     instructions = """
-       You are a triage agent. Your goal is to answer questions and redirect message according to their intent. You have at your disposition 2 tools but can only use ONE:
-        1. cqa_api: to answer customer questions such as procedures and FAQs.
-        2. clu_api: to extract the intent of the message.
-        You must use the ONE of the tools to perform your task. You should only use one tool at a time, and do NOT chain the tools together. Only if the tools are not able to provide the information, you can answer according to your general knowledge.
-        - When you return answers from the cqa_api, format the response as JSON: {"type": "CQA", "result": "(cqa_response)"} where cqa_response is the EXACT answer from cqa_api without rewriting.
-        - When you return answers from the clu_api, format the response as JSON: {"type": "CLU", "result": {clu response}}, where clu_response is the full JSON response from the clu_api without rewriting or removing any info. Return immediately. Do not call the cqa_api afterwards
-        To call the clu_api, the following parameters values should be used in the payload:
-        - 'projectName': value must be 'conv-assistant-clu'
-        - 'deploymentName': value must be 'clu-m1-d1'
-        - 'text': must be the input from the user.
-        - 'api-version': value must be "2023-04-01"
+        You are a triage agent. Your goal is to answer questions and redirect message according to their intent. You have at your disposition 2 tools but can only use ONE:
+            1. cqa_api: to answer customer questions such as procedures and FAQs.
+            2. clu_api: to extract the intent of the message.
+
+        You must use the ONE of the tools to perform your task. 
+        You should only use one tool at a time, and do NOT chain the tools together. 
+        Only if the tools are not able to provide the information, you can answer according to your general knowledge. 
+        You must return the full API response for either tool and ensure it's a valid JSON.
+
+            - When you return answers from the clu_api, format the response as JSON: {"type": "clu_result", "response": {clu_response}}.
+            clu_response is the full JSON API response from the clu_api without rewriting or removing any info. 
+            Return immediately. Do not call the cqa_api afterwards.
+            To call the clu_api, the following parameters values should be used in the payload:
+                - 'projectName': value must be 'conv-assistant-clu'
+                - 'deploymentName': value must be 'clu-m1-d1'
+                - 'text': must be the input from the user.
+                - 'api-version': must be "2023-04-01"
+            - When you return answers from the cqa_api, format the response as JSON: {"type": "cqa_result", "response": {cqa_response}}.
+            cqa_response is the full JSON API response from the cqa_api without rewriting or removing any info. Return immediately.
         """
 
     instructions = bind_parameters(instructions, config)
